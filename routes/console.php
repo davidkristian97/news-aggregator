8bab4ab977dba2de067c44c47a424c680c584633
davidkristian97 <davidkristianbisnis@gmail.com>
2026-06-25 12:03:26 +0700

    Add scheduler for recurring data fetch

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Requires `* * * * * php artisan schedule:run` in the server crontab to actually fire.
+Schedule::command('articles:fetch')->everyTwoHours()->withoutOverlapping();
